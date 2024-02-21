f28db9472a8ced069a7b391cf48f1662f352a416
Wolf <32619379+AWoLf98@users.noreply.github.com>
2024-02-22 01:57:01 +0200

    add class timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,9 +4,74 @@ import iziToast from 'izitoast';
 import 'flatpickr/dist/flatpickr.min.css';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const UPDATE_TIMER = 1000;
+const UPDATE_TIMER_INTERVAL = 1000;
+
+const datetime_pickr = document.querySelector('input#datetime-picker');
+const start = document.querySelector('button[data-start]');
 
 let userSelectedDate = '';
+
+class Timer {
+  static Timer(idInteval) {
+    this.idInteval = idInteval;
+  }
+
+  static startTimer() {
+    const timeLeft = Date.parse(userSelectedDate) - Date.now();
+    
+    if (timeLeft < 0) {
+      iziToast.show({
+        icon: 'fa-regular fa-circle-xmark',
+        close: false,
+        iconColor: '#fff',
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        progressBarColor: '#B51B1B',
+        messageColor: '#fff',
+        message: 'Please choose a date in the future',
+      });
+      return;
+    }
+
+    this.idInteval = setInterval(Timer.updateTimer, UPDATE_TIMER_INTERVAL);
+    console.log(this.idInteval)
+  }
+
+
+  static *convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  static *addLeadingZero(value) {
+    return value.padStart(2, '0');
+  }
+
+  static updateTimer() {
+    console.log('Start interval');
+    console.log(Timer.idInteval);
+    // console.log(clearInterval(idInteval));
+    // if(timeLeft - UPDATE_TIMER_INTERVAL < 0) {
+    //   clearInterval(idInteval);
+    // }
+    // const { days, hours, minutes, seconds } = this.convertMs(timeLeft);
+    
+  }
+}
 
 const options = {
   enableTime: true,
@@ -19,35 +84,7 @@ const options = {
   },
 };
 
-const datetime_flatpickr = flatpickr('#datetime-picker', options);
-const datetime_pickr = document.querySelector('#datetime-picker');
+const datetime_flatpickr = flatpickr('input#datetime-picker', options);
 
-const start = document.querySelector('button[data-start]');
+start.addEventListener('click', Timer.startTimer);
 
-start.addEventListener('click', startTimer);
-datetime_pickr.addEventListener('input', readInput);
-
-function startTimer() {
-    if((Date.parse(userSelectedDate) - Date.now()) < 0){
-        iziToast.show({
-          icon: 'fa-regular fa-circle-xmark',
-          iconColor: '#fff',
-          position: 'topRight',
-          backgroundColor: '#EF4040',
-          progressBarColor: '#B51B1B',
-          messageColor: '#fff',
-          message: 'Please choose a date in the future',
-        });
-        return;
-    }
-}
-
-function readInput() {
-    // // alert(userSelectedDate);
-    // console.log(userSelectedDate);
-    // console.log(Date.parse(userSelectedDate));
-}
-
-class Timer {
-
-}
