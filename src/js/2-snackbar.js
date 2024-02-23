5e067f2c728dd46a75b7d4d676986681175a9464
Wolf <32619379+AWoLf98@users.noreply.github.com>
2024-02-23 02:01:40 +0200

    add some js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,19 @@
 import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form'); 
+
+form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    iziToast.show({
+        icon: 'fa-regular fa-circle-xmark',
+        close: false,
+        iconColor: '#fff',
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        progressBarColor: '#B51B1B',
+        messageColor: '#fff',
+        message: 'Please choose a date in the future',
+      });
+})
