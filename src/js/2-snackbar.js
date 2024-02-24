64a60e0d58a4502b520c6d4b183834d4ad44f53e
Wolf <32619379+AWoLf98@users.noreply.github.com>
2024-02-24 17:11:03 +0200

    end 2 task

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,18 +2,42 @@ import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form'); 
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', (event)=> {
-    event.preventDefault();
-    iziToast.show({
-        icon: 'fa-regular fa-circle-xmark',
-        close: false,
-        iconColor: '#fff',
-        position: 'topRight',
-        backgroundColor: '#EF4040',
-        progressBarColor: '#B51B1B',
-        messageColor: '#fff',
-        message: 'Please choose a date in the future',
-      });
-})
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = form.elements['delay'].value;
+  const radioValue = form.elements['state'].value;
+
+  const notificationPromise  = new Promise((resolve, rejected) => {
+    setTimeout(() => {
+        if (radioValue === 'fulfilled') {
+          return resolve(delay);
+        } else if (radioValue === 'rejected') {
+          return rejected(delay);
+        }
+    }, delay)
+  
+  });
+
+  notificationPromise
+  .then(delay => {
+    iziShow('#59A10D', `✅ Fulfilled promise in ${delay}ms`)
+  })
+  .catch(delay => {
+    iziShow('#EF4040', `❌ Rejected promise in ${delay}ms`)
+  })
+});
+
+
+function iziShow(color, message) {
+  iziToast.show({
+    color: color,
+    close: false,
+    position: 'topRight',
+    backgroundColor: color,
+    messageColor: '#fff',
+    message: message,
+  });
+}
